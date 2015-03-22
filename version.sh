68bd1fb71e8fef4339cc0dc0fd5baa0c1d1f9724
marc3825 <marc.tassetti@gmail.com>
2015-03-22 22:04:38 +0100

    Cleaned some old commented code

--- version.sh
+++ version.sh
@@ -36,8 +36,6 @@ case $1 in
         else
             if [ -f .version/$2.$3 ] ; then
                 cp -f .version/$2.1 $2
-                #patch -p0 < .version/$2.{`seq -s"," 2 $3`} 
-                #patch $2 .version/$2.{`seq -s"," 2 $3`}
                 for n in `seq 2 $3` ; do patch $2 .version/$2.$n; done
                 echo -e "Checked out version: $3"
             else
